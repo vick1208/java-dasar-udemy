3d04797824353149f31171cf0e9bd733e85ca362
Vicky AS <vickyhuang808@gmail.com>
2023-04-12 16:36:28 +0700

    modify file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("KONNICHIWA");
-        System.out.println("This is the github test");
     }
 }
