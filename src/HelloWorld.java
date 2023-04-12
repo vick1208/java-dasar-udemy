0997ea6a1046703c486ed1b1becb91c762d9334a
Vicky AS <vickyhuang808@gmail.com>
2023-04-12 16:34:53 +0700

    test commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("KONNICHIWA");
+        System.out.println("This is the github test");
     }
 }
